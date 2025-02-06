106f183d8c0e6a77703b2f551a6b1c37366de731
CJ <cherley95@hotmail.fr>
2025-02-06 16:48:48 +0100

    nouvel url server

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 module.exports = function (app) {
     app.use(
         "/api",
         createProxyMiddleware({
-            target: "https://donymusic-server.vercel.app",
+            target: "https://don-server.vercel.app",
             changeOrigin: true,
             secure: false,
         })
